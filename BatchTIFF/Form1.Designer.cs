347bfab7c1f04c419d0a5a70e721f74ea5b91b99
Alexey Kolesnikow <alekseykw@gmail.com>
2019-11-17 21:50:07 +0700

    convert and round coords

---
+++ BatchTIFF/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace BatchTIFF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запустить обработку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Указать папку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 122);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(391, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(173, 41);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(143, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Округлять координаты";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 209);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ BatchTIFF/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using OSGeo.GDAL;
+using OSGeo.OSR;
+
+namespace BatchTIFF
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Gdal.AllRegister();
+            string[] tiff_files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.tif", SearchOption.AllDirectories);
+            int files_count = tiff_files.Length;
+            List<string> tab_strings;
+            var regex = new Regex(Regex.Escape(","));
+            foreach (string tiff_file in tiff_files)
+            {
+                tab_strings = new List<string>();
+                string file_path = Path.GetDirectoryName(tiff_file);
+                string file_name = Path.GetFileName(tiff_file);
+                Dataset tiff_image = Gdal.Open(tiff_file, Access.GA_ReadOnly);
+                Driver drv = tiff_image.GetDriver();
+                //MessageBox.Show("Corner Coordinates:");
+                //MessageBox.Show("  Upper Left (" + GDALInfoGetPosition(tiff_image, 0.0, 0.0) + ")"+ "(0.0, 0.0)");
+                //MessageBox.Show("  Lower Right (" + GDALInfoGetPosition(tiff_image, tiff_image.RasterXSize, tiff_image.RasterYSize) + ")" + "(" + tiff_image.RasterXSize.ToString() + ", " + tiff_image.RasterYSize.ToString() + ")");
+                //MessageBox.Show("  Upper Right (" + GDALInfoGetPosition(tiff_image, tiff_image.RasterXSize, 0.0) + ")" + "(" + tiff_image.RasterXSize.ToString() + ", 0.0)");
+                //MessageBox.Show("  Lower Left (" + GDALInfoGetPosition(tiff_image, 0.0, tiff_image.RasterYSize) + ")"+ "(0.0, "+ tiff_image.RasterYSize.ToString() + ")");
+                //MessageBox.Show("  Center (" + GDALInfoGetPosition(tiff_image, tiff_image.RasterXSize / 2, tiff_image.RasterYSize / 2) + ")");
+                tab_strings.Add("!table");
+                tab_strings.Add("!version 300");
+                tab_strings.Add("!charset WindowsCyrillic");
+                tab_strings.Add("");
+                tab_strings.Add("Definition Table");
+                tab_strings.Add("  File \"" + file_name + "\"");
+                tab_strings.Add("  Type \"RASTER\"");
+                string[] coords = GDALInfoGetPosition(tiff_image, 0.0, 0.0).Split(new string[] {", " }, StringSplitOptions.None);
+                string X = coords[0];
+                string Y = coords[1];
+                if (checkBox1.Checked)
+                {
+                    X = X.Replace(',', '.');
+                    int X_int = System.Convert.ToInt32(X);
+                    X = X_int.ToString() + ".0";
+                    Y = Y.Replace(',', '.');
+                    int Y_int = System.Convert.ToInt32(Y);
+                    Y = Y_int.ToString() + ".0";
+                }
+                else
+                {
+                    X = X.Replace(',', '.');
+                    Y = Y.Replace(',', '.');
+                }
+                tab_strings.Add("  ("+X+","+Y+ ") (0,0) Label \"Точка 1\",");
+                coords = GDALInfoGetPosition(tiff_image, tiff_image.RasterXSize, 0.0).Split(new string[] { ", " }, StringSplitOptions.None);
+                X = coords[0];
+                Y = coords[1];
+                if (checkBox1.Checked)
+                {
+                    X = X.Replace(',', '.');
+                    int X_int = System.Convert.ToInt32(X);
+                    X = X_int.ToString() + ".0";
+                    Y = Y.Replace(',', '.');
+                    int Y_int = System.Convert.ToInt32(Y);
+                    Y = Y_int.ToString() + ".0";
+                }
+                else
+                {
+                    X = X.Replace(',', '.');
+                    Y = Y.Replace(',', '.');
+                }
+                tab_strings.Add("  ("+ X+","+Y + ") ("+ tiff_image.RasterXSize.ToString()+ ",0) Label \"Точка 2\",");
+                coords = GDALInfoGetPosition(tiff_image, 0.0, tiff_image.RasterYSize).Split(new string[] { ", " }, StringSplitOptions.None);
+                X = coords[0];
+                Y = coords[1];
+                if (checkBox1.Checked)
+                {
+                    X = X.Replace(',', '.');
+                    int X_int = System.Convert.ToInt32(X);
+                    X = X_int.ToString() + ".0";
+                    Y = Y.Replace(',', '.');
+                    int Y_int = System.Convert.ToInt32(Y);
+                    Y = Y_int.ToString() + ".0";
+                }
+                else
+                {
+                    X = X.Replace(',', '.');
+                    Y = Y.Replace(',', '.');
+                }
+                tab_strings.Add("  ("+ X+","+Y + ") (0,"+ tiff_image.RasterYSize.ToString()+ ")  Label \"Точка 3\"");
+                tab_strings.Add("  CoordSys NonEarth Units \"m\"");
+                tab_strings.Add("  Units \"m\"");
+                //string projection = tiff_image.GetProjectionRef();
+                //if (projection != null)
+                //{
+                //    SpatialReference srs = new SpatialReference(null);
+                //    if (srs.ImportFromWkt(ref projection) == 0)
+                //    {
+                //        string wkt;
+                //        srs.ExportToPrettyWkt(out wkt, 0);
+                //        MessageBox.Show("Coordinate System is: "+ wkt);
+                //    }
+                //    else
+                //    {
+                //        MessageBox.Show("Coordinate System is: "+ projection);
+                //    }
+                //}
+                File.WriteAllLines(Path.GetDirectoryName(tiff_file)+"\\"+Path.GetFileNameWithoutExtension(tiff_file) + ".TAB", tab_strings);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                label1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private static string GDALInfoGetPosition(Dataset ds, double x, double y)
+        {
+            double[] adfGeoTransform = new double[6];
+            double dfGeoX, dfGeoY;
+            ds.GetGeoTransform(adfGeoTransform);
+
+            dfGeoX = adfGeoTransform[0] + adfGeoTransform[1] * x + adfGeoTransform[2] * y;
+            dfGeoY = adfGeoTransform[3] + adfGeoTransform[4] * x + adfGeoTransform[5] * y;
+
+            return dfGeoX.ToString() + ", " + dfGeoY.ToString();
+        }
+    }
+
+}
